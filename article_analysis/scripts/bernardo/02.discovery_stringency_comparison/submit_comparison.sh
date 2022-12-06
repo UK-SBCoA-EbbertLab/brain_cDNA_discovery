6fca401c1c188068db8945e2db6c78df3c3bf5cc
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2022-12-06 17:02:15 -0500

    Last update before adding Mark and Maddy

--- article_analysis/scripts/bernardo/02.discovery_stringency_comparison/submit_comparison.sh
+++ article_analysis/scripts/bernardo/02.discovery_stringency_comparison/submit_comparison.sh
@@ -7,33 +7,8 @@
 
 
 ## Loose run comparison, bambu NDR = 1
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_first_half_loose_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_second_half_loose_annotation_novel_only.gtf "ANNOTATED" second_half_annotation_loose.tsv
+sbatch run_identify_novel_transcripts.ogs ../../../data/bernardo/processed/99.other/create_annotations/novel_only_annotations/merged_first_half_loose_annotation_novel_only.gtf \
+    ../../../data/bernardo/processed/99.other/create_annotations/novel_only_annotations/merged_second_half_loose_annotation_novel_only.gtf "ANNOTATED" second_half_annotation_loose.tsv
 
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_second_half_loose_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_first_half_loose_annotation_novel_only.gtf "ANNOTATED" first_half_annotation_loose.tsv
-
-
-## Moderate run comparison, bambu NDR = 0.5
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_first_half_moderate_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_second_half_moderate_annotation_novel_only.gtf "ANNOTATED" second_half_annotation_moderate.tsv
-
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_second_half_moderate_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_first_half_moderate_annotation_novel_only.gtf "ANNOTATED" first_half_annotation_moderate.tsv
-
-
-## Stringent run comparison, bambu NDR = 0.1
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_first_half_stringent_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_second_half_stringent_annotation_novel_only.gtf "ANNOTATED" second_half_annotation_stringent.tsv
-
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_second_half_stringent_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_first_half_stringent_annotation_novel_only.gtf "ANNOTATED" first_half_annotation_stringent.tsv
-
-
-## Super stringent run comparison, bambu NDR = 0.01
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_first_half_super_stringent_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_second_half_super_stringent_annotation_novel_only.gtf "ANNOTATED" second_half_annotation_super_stringent.tsv
-
-sbatch run_identify_novel_transcripts.ogs ../../data/processed/novel_only_annotations/merged_second_half_super_stringent_annotation_novel_only.gtf \
-    ../../data/processed/novel_only_annotations/merged_first_half_super_stringent_annotation_novel_only.gtf "ANNOTATED" first_half_annotation_super_stringent.tsv
-
+sbatch run_identify_novel_transcripts.ogs ../../../data/bernardo/processed/99.other/create_annotations/novel_only_annotations/merged_second_half_loose_annotation_novel_only.gtf \
+    ../../../data/bernardo/processed/99.other/create_annotations/novel_only_annotations/merged_first_half_loose_annotation_novel_only.gtf "ANNOTATED" first_half_annotation_loose.tsv
