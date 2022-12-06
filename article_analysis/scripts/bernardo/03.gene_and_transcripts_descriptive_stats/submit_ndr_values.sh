fb5e205ceb213372a95901a4b239d4575f1a071e
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2022-12-06 09:34:54 -0500

    cleaning up scripts/bernardo/03 directory

--- article_analysis/scripts/bernardo/03.gene_and_transcripts_descriptive_stats/submit_ndr_values.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-singularity exec /scratch/bag222/cDNA_pipeline/singularity_container/cdna_nanopore_pipe.sif ./ndr_values.R ../../data/raw/merged_aged_stringent/bambu_discovery/final_discovery.RDS merged_aged_stringent_ndr.tsv
