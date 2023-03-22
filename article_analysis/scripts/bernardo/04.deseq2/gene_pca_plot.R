631e9df449f4ea632da89f48f965237de54cbb24
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-03-22 08:31:58 -0400

    making figures prettier

--- article_analysis/scripts/bernardo/04.deseq2/gene_pca_plot.R
+++ article_analysis/scripts/bernardo/04.deseq2/gene_pca_plot.R
@@ -3,6 +3,7 @@
 ## Import Libraries
 library("DESeq2")
 library("ggplot2")
+#library("svglite")
 
 
 
@@ -64,28 +65,74 @@ pcaData_100 <- plotPCA(vsd_100, intgroup=c("condition", "sex"), returnData=TRUE)
 percentVar_100 <- round(100 * attr(pcaData_100, "percentVar"))
 
 
-# PDF device
-pdf("../../../figures/bernardo/02.quality_control/gene_level_PCAs.pdf")
-
 ## Plot PCAs
-ggplot(pcaData, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar[1],"% variance")) +  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-  coord_fixed() + ggtitle("Unfiltered")
+unfiltered_plot = ggplot(pcaData, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar[1],"% variance")) +  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  coord_fixed() + theme(
+         panel.background = element_rect(fill='transparent'),
+         plot.background = element_rect(fill='transparent', color=NA),
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         legend.background = element_rect(fill='transparent'),
+         legend.box.background = element_rect(fill='transparent')
+       )
 
-ggplot(pcaData, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar[1],"% variance")) +  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-  coord_fixed() + ggtitle("Unfiltered")
+ten_plot = ggplot(pcaData_10, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar_10[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_10[2],"% variance")) +
+  coord_fixed() + theme(
+         panel.background = element_rect(fill='transparent'),
+         plot.background = element_rect(fill='transparent', color=NA),
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         legend.background = element_rect(fill='transparent'),
+         legend.box.background = element_rect(fill='transparent')
+       )
 
-ggplot(pcaData_10, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar_10[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_10[2],"% variance")) +
-  coord_fixed() + ggtitle("10+ counts in 4/12 samples")
+twenty_five_plot = ggplot(pcaData_25, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar_25[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_25[2],"% variance")) +
+  coord_fixed() + theme(
+         panel.background = element_rect(fill='transparent'),
+         plot.background = element_rect(fill='transparent', color=NA),
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         legend.background = element_rect(fill='transparent'),
+         legend.box.background = element_rect(fill='transparent')
+       )
 
-ggplot(pcaData_25, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar_25[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_25[2],"% variance")) +
-  coord_fixed() + ggtitle("25+ counts in 4/12 samples")
+fifty_plot = ggplot(pcaData_50, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=1) +  xlab(paste0("PC1: ",percentVar_50[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_50[2],"% variance")) +
+  coord_fixed() + theme(
+         panel.background = element_rect(fill='transparent'),
+         plot.background = element_rect(fill='transparent', color=NA),
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         legend.background = element_rect(fill='transparent'),
+         legend.box.background = element_rect(fill='transparent')
+       )
 
-ggplot(pcaData_50, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar_50[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_50[2],"% variance")) +
-  coord_fixed() + ggtitle("50+ counts in 4/12 samples")
+hundred_plot = ggplot(pcaData_100, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=1.5) +  xlab(paste0("PC1: ",percentVar_100[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_100[2],"% variance")) +
+  coord_fixed() + theme(
+         text = element_text(size=5),
+         panel.background = element_rect(fill='transparent'),
+         plot.background = element_rect(fill='transparent', color=NA),
+         plot.margin = unit(c(0,0,0,0), "mm"),
+         panel.grid.major = element_blank(), 
+         panel.grid.minor = element_blank(),
+         panel.border = element_rect(color = "black", fill=NA, linewidth=1),
+         legend.background = element_rect(fill='transparent', color=NA),
+         legend.box.background = element_rect(fill='transparent', color=NA),
+         legend.text = element_text(color="black", size=4),
+         legend.direction = "horizontal",
+         legend.position="top",
+         legend.justification = "left",
+         legend.spacing.x = unit(0.2, "mm"),
+         legend.spacing.y = unit(0.05, "mm"),
+         legend.key.height = unit(0, "mm"),
+         legend.key.width = unit(4, "mm"),
+         legend.margin = margin(-3, 2, -3, 2, "mm"),
+         legend.key=element_blank()
+       )
 
-ggplot(pcaData_100, aes(PC1, PC2, color=condition, shape=sex)) +  geom_point(size=3) +  xlab(paste0("PC1: ",percentVar_100[1],"% variance")) +  ylab(paste0("PC2: ",percentVar_100[2],"% variance")) +
-  coord_fixed() + ggtitle("100+ counts in 4/12 samples")
-
-dev.off()
+ggsave("../../../figures/bernardo/02.quality_control/pca_unfiltered_gene.png", unfiltered_plot, bg="transparent", dpi=1200, units="mm", width=59, height=64)
+ggsave("../../../figures/bernardo/02.quality_control/pca_10_gene.png", ten_plot, bg="transparent", dpi=1200, units="mm", width=59, height=64)
+ggsave("../../../figures/bernardo/02.quality_control/pca_25_gene.png", twenty_five_plot, bg="transparent", dpi=1200, units="mm", width=59, height=64)
+ggsave("../../../figures/bernardo/02.quality_control/pca_50_gene.png", fifty_plot, bg="transparent", dpi=1200, units="mm", width=59, height=64)
+ggsave("../../../figures/bernardo/02.quality_control/pca_100_gene.png", hundred_plot, bg="transparent", dpi=1200, units="mm", width=59, height=64)
 
 
