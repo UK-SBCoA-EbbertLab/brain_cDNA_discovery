b6245edff9502ea5b30bfcda91f1583df2745457
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-05-02 16:56:05 -0400

    final updates to cDNA pipeline

--- cDNA_pipeline/workflow/submission_scripts/ad_vs_ct_pilot_study_february_2023_GRCh38-107_discovery.sh
+++ cDNA_pipeline/workflow/submission_scripts/ad_vs_ct_pilot_study_february_2023_GRCh38-107_discovery.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-nextflow ../main.nf --ont_reads_fq "/scratch/bag222/data/ont_data/09-02-2022_uky_6ad_6ct/*.fastq" \
-    --ont_reads_txt "/scratch/bag222/data/ont_data/09-02-2022_uky_6ad_6ct/*.txt" \
+nextflow ../main.nf --ont_reads_fq "../../../../../../../scratch/bag222/data/ont_data/09-02-2022_uky_6ad_6ct/*.fastq" \
+    --ont_reads_txt "../../../../../../scratch/bag222/data/ont_data/09-02-2022_uky_6ad_6ct/*.txt" \
     --ref "../../references/Homo_sapiens.GRCh38_ERCC.fa" \
     --annotation "../../references/Homo_sapiens.GRCh38.107_ERCC.gtf" \
     --housekeeping "../../references/hg38.HouseKeepingGenes.bed" \
--- cDNA_pipeline/workflow/submission_scripts/legacy_submission_scripts/submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-./merged_samples_stringent_discovery_GRCH38-107.sh 
-./merged_first_half_loose_discovery_GRCH38-107.sh
-./merged_second_half_loose_discovery_GRCH38-107.sh
-
