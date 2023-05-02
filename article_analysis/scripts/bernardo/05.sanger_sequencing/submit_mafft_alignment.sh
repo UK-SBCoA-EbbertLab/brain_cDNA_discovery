089d3dbee9810f583f1febb1f679414da2df2699
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-05-02 15:23:47 -0400

    added mafft analysis for sanger sequencing

---
+++ article_analysis/scripts/bernardo/05.sanger_sequencing/submit_mafft_alignment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+sbatch mafft_alignment.sbatch ../../../data/bernardo/processed/05.sanger_sequencing/sanger_sequencing_fasta_files/MAFFT_alignment_BambuTx1845_forward.fa > \
+    ../../../data/bernardo/processed/05.sanger_sequencing/mafft_alignment_output/MAFFT_alignment_BambuTx1845_forward_OUTPUT.txt
