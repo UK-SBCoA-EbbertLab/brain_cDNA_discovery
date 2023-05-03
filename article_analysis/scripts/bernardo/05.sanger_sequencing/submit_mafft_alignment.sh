5aa4ead8ac3a35004110770d02202c5a2a0c1325
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-05-03 16:32:04 -0400

    big bernardo update

--- article_analysis/scripts/bernardo/05.sanger_sequencing/submit_mafft_alignment.sh
+++ article_analysis/scripts/bernardo/05.sanger_sequencing/submit_mafft_alignment.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 
-sbatch mafft_alignment.sbatch ../../../data/bernardo/processed/05.sanger_sequencing/sanger_sequencing_fasta_files/MAFFT_alignment_BambuTx1845_forward.fa > \
-    ../../../data/bernardo/processed/05.sanger_sequencing/mafft_alignment_output/MAFFT_alignment_BambuTx1845_forward_OUTPUT.txt
+sbatch mafft_alignment.sbatch ../../../data/bernardo/processed/05.sanger_sequencing/sanger_sequencing_fasta_files/MAFFT_alignment_BambuTx1845_forward.fa \
+    ../../../data/bernardo/processed/05.sanger_sequencing/mafft_alignment_results/MAFFT_alignment_BambuTx1845_forward_OUTPUT.txt
+
+sbatch mafft_alignment.sbatch ../../../data/bernardo/processed/05.sanger_sequencing/sanger_sequencing_fasta_files/MAFFT_alignment_BambuTx1845_reverse.fa \
+    ../../../data/bernardo/processed/05.sanger_sequencing/mafft_alignment_results/MAFFT_alignment_BambuTx1845_reverse_OUTPUT.txt
+
+sbatch mafft_alignment.sbatch ../../../data/bernardo/processed/05.sanger_sequencing/sanger_sequencing_fasta_files/MAFFT_alignment_BambuTx2703_forward.fa \
+    ../../../data/bernardo/processed/05.sanger_sequencing/mafft_alignment_results/MAFFT_alignment_BambuTx2703_forward_OUTPUT.txt
+
+sbatch mafft_alignment.sbatch ../../../data/bernardo/processed/05.sanger_sequencing/sanger_sequencing_fasta_files/MAFFT_alignment_BambuTx2703_reverse.fa \
+    ../../../data/bernardo/processed/05.sanger_sequencing/mafft_alignment_results/MAFFT_alignment_BambuTx2703_reverse.fa
