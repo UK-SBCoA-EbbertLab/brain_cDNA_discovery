fc6640a615f4dbcd145384a816af08817a6e01d2
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-11-04 11:35:41 -0400

    update BA9 specific analysis

---
+++ article_analysis/scripts/1st_rebuttal_scripts/discovery_comparison_bambu_BA9/gffcompare_ours_107_vs_others.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+## OURS ALL vs GTEx ENSEMBL 107 ALL
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_107_NEW_ALL.gtf \
+    ../../../data/processed/other/novel_only_annotations/all_novel_bambu.gtf \
+    -o OURS_ALL_vs_GTEx_BA9_ENSEMBL_107_ALL
+
+## OURS ALL vs GTEx ENSEMBL 88 ALL
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_88_NEW_ALL.gtf \
+    ../../../data/processed/other/novel_only_annotations/all_novel_bambu.gtf \
+    -o OURS_ALL_vs_GTEx_BA9_ENSEMBL_88_ALL
+
+## OURS ALL vs GTEx ENSEMBL 107 HF
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_107_NEW_HF.gtf \
+    ../../../data/processed/other/novel_only_annotations/all_novel_bambu.gtf \
+    -o OURS_ALL_vs_GTEx_BA9_ENSEMBL_107_HF
+
+## OURS ALL vs GTEx ENSEMBL 88 HF
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_88_NEW_HF.gtf \
+    ../../../data/processed/other/novel_only_annotations/all_novel_bambu.gtf \
+    -o OURS_ALL_vs_GTEx_BA9_ENSEMBL_88_HF
+
+
+## OURS HF vs GTEx ENSEMBL 107 ALL
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_107_NEW_ALL.gtf \
+    ../../../data/processed/other/novel_only_annotations/high_confidence_novel_bambu.gtf \
+    -o OURS_HF_vs_GTEx_BA9_ENSEMBL_107_ALL
+
+## OURS HF vs GTEx ENSEMBL 88 ALL
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_88_NEW_ALL.gtf \
+    ../../../data/processed/other/novel_only_annotations/high_confidence_novel_bambu.gtf \
+    -o OURS_HF_vs_GTEx_BA9_ENSEMBL_88_ALL
+
+## OURS HF vs GTEx ENSEMBL 107 HF
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_107_NEW_HF.gtf \
+    ../../../data/processed/other/novel_only_annotations/high_confidence_novel_bambu.gtf \
+    -o OURS_HF_vs_GTEx_BA9_ENSEMBL_107_HF
+
+## OURS HF vs GTEx ENSEMBL 88 HF
+singularity exec ../../../../singularity_containers/nanopore.sif gffcompare -r \
+    ../../../data/processed/1st_rebuttal/discovery_comparison_bambu_BA9/GTEx_BA9_ENSEMBL_88_NEW_HF.gtf \
+    ../../../data/processed/other/novel_only_annotations/high_confidence_novel_bambu.gtf \
+    -o OURS_HF_vs_GTEx_BA9_ENSEMBL_88_HF
