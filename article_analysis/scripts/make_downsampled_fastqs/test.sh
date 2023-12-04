3272c353d94f73247513b37b1105a8779251558b
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2023-12-04 11:10:34 -0500

    update

--- article_analysis/scripts/make_downsampled_fastqs/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sbatch ./subsample_fastqs_and_seq_summary.ogs ../../../../../../scratch/bag222/data/ont_data/test_data/PAM05748_1271_test.fastq \
-    ../../../../../../scratch/bag222/data/ont_data/test_data/PAM05748_1271_test.txt
